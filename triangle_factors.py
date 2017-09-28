1b214ea291cebf2959aa10a3be2ca0de2dabc307
Samuel Teich <steich@gmail.com>
2017-09-28 13:39:19 -0500

    Accidentally deleted some stuff... Someone else should fix my mistakes.

---
+++ triangle_factors.py
@@ -0,0 +1,13 @@
+###FIX ME!!!###
+counter = 1
+divisor = 0
+while divisor < 500:
+  #Something got deleted here.
+  triNumber = sum(range(1, counter + 1))
+  for divisor in range(1, triNumber + 1):
+    #Something else got deleted here. 
+      divisors += 1
+      print(triNumber)
+      counter += 1
+      #Here as well.
+      #One of these comments is a lie.
